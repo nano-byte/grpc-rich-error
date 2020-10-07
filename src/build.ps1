bcb7d385aefe3452227eb3aee42f4e3884bbf9dd
Bastian Eicher <bastian@eicher.net>
2020-10-07 18:11:02 +0200

    Tweaked build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -2,15 +2,6 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-function Find-MSBuild {
-    if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
-        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value -version 16.5
-        if ($vsDir) {
-            if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
-        }
-    }
-}
-
 function Run-DotNet {
     if (Get-Command dotnet -ErrorAction SilentlyContinue) {
         dotnet @args
@@ -20,18 +11,7 @@ function Run-DotNet {
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
-function Run-MSBuild {
-    $msbuild = Find-MSBuild
-    if ($msbuild) {
-        . $msbuild @args
-        if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
-    } else {
-        Run-DotNet msbuild @args
-    }
-}
-
-# Build
 if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
-Run-MSBuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
+Run-DotNet msbuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
 
 popd

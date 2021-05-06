35491e0bc7165122628d1ab9b2128500e97d084b
Bastian Eicher <bastian@eicher.net>
2021-05-06 11:28:24 +0200

    Updated build scripts to use .NET 5.0 SDK

--- src/build.ps1
+++ src/build.ps1
@@ -6,12 +6,13 @@ function Run-DotNet {
     if (Get-Command dotnet -ErrorAction SilentlyContinue) {
         dotnet @args
     } else {
-        ..\0install.ps1 run --batch --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml @args
+        ..\0install.ps1 run --batch --version 5.0..!5.1 https://apps.0install.net/dotnet/core-sdk.xml @args
     }
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
+# Build
 if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
-Run-DotNet msbuild /v:Quiet /t:Restore /t:Build $ci /p:Configuration=Release /p:Version=$Version
+Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version $ci
 
 popd
--- src/build.sh
+++ src/build.sh
@@ -6,8 +6,8 @@ cd `dirname $0`
 if command -v dotnet > /dev/null 2> /dev/null; then
     dotnet="dotnet"
 else
-    dotnet="../0install.sh run --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml"
+    dotnet="../0install.sh run --version 5.0..!5.1 https://apps.0install.net/dotnet/core-sdk.xml"
 fi
 
 # Build
-$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre}
+$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True}

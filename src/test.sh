db23771ae3cf66e77d7e67e1541cb24468f2fbdf
Bastian Eicher <bastian@eicher.net>
2021-11-29 21:29:35 +0100

    Updated build scripts to use .NET SDK 6.0

--- src/test.sh
+++ src/test.sh
@@ -6,8 +6,8 @@ cd `dirname $0`
 if command -v dotnet > /dev/null 2> /dev/null; then
     dotnet="dotnet"
 else
-    dotnet="../0install.sh run --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml"
+    dotnet="../0install.sh run --version 6.0.. https://apps.0install.net/dotnet/sdk.xml"
 fi
 
 # Unit tests (without .NET Framework)
-$dotnet test --no-build --logger junit --configuration Release --framework netcoreapp3.1 UnitTests/UnitTests.csproj
+$dotnet test --no-build --logger junit --configuration Release --framework net6.0 UnitTests/UnitTests.csproj
